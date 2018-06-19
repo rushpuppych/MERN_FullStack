95a2560deb289cb58e0fae9a2a9ef469e2894dcd
RushPuppy <rushpuppych@users.noreply.github.com>
2018-06-19 19:50:15 +0200

    Adding CSS Modules to Demo

--- app/components/Demo/Demo.js
+++ app/components/Demo/Demo.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import {Row, Col, Card, CardTitle} from 'react-materialize';
+import {Row, Col, Card} from 'react-materialize';
 import Theme from '../../assets/themes/Default';
 import Lang from './DemoLang';
+import Style from './Demo.css';
 
 const demo = (props) => (
     <Row>
         <Col m={4} s={4}>
             <Card className={Theme.components.demo.backgroundColor} textClassName={Theme.components.demo.textColor} title="Test Titel" actions={[<a href='/test'>{Lang.link}</a>]}>
                 {Lang.content}
+                <span className={Style.DemoCssTest}>CSS Module Test</span>
             </Card>
         </Col>        
     </Row>
