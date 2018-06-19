38f73b3c59b5d089c503c6dfacec61e604693fff
RushPuppy <rushpuppych@users.noreply.github.com>
2018-06-19 19:45:26 +0200

    Modulize Controller Code

--- api/controllers/DemoController.js
+++ api/controllers/DemoController.js
@@ -3,27 +3,24 @@
 // Include Models
 //const Note = require('../models/note.model.js');
 
-// Create and Save a new Note
-exports.create = (req, res) => {
-  res.json({'message': 'create'});
-};
+module.exports = {
+  create: (req, res) => {
+    res.json({'message': 'create'});
+  },
 
-// Retrieve and return all notes from the database.
-exports.findAll = (req, res) => {
-  res.json({'message': 'findAll_demo'});
-};
+  findAll: (req, res) => {
+    res.json({'message': 'findAll_demo'});
+  },
 
-// Find a single note with a noteId
-exports.findOne = (req, res) => {
-  res.json({'message': 'findOne'});
-};
+  findOne: (req, res) => {
+    res.json({'message': 'findOne'});
+  },
 
-// Update a note identified by the noteId in the request
-exports.update = (req, res) => {
-  res.json({'message': 'update'});
-};
+  update: (req, res) => {
+    res.json({'message': 'update'});
+  },
 
-// Delete a note with the specified noteId in the request
-exports.delete = (req, res) => {
-  res.json({'message': 'delete'});
+  delete: (req, res) => {
+    res.json({'message': 'delete'});
+  }
 };
--- api/routes/DemoRoutes.js
+++ api/routes/DemoRoutes.js
@@ -1,12 +1,12 @@
 
 module.exports = (app) => {
-    const controller = require('../controllers/DemoController.js');
+    const Controller = require('../controllers/DemoController.js');
 
     // Create Demo
-    app.post('/demo', controller.create);
+    app.post('/demo', Controller.create);
 
     // Get All Back
-    app.get('/demo', controller.findAll);
+    app.get('/demo', Controller.findAll);
 
     /**
      * @api {post} /demo/:id Request Demo Information of specific Demo Object
@@ -18,11 +18,11 @@ module.exports = (app) => {
      * @apiSuccess {String} firstname Firstname of the User.
      * @apiSuccess {String} lastname  Lastname of the User.
      */
-    app.get('/demo/:demoId', controller.findOne);
+    app.get('/demo/:demoId', Controller.findOne);
 
     // Update a Note with noteId
-    app.put('/demo/:demoId', controller.update);
+    app.put('/demo/:demoId', Controller.update);
 
     // Delete a Note with noteId
-    app.delete('/demo/:demoId', controller.delete);
+    app.delete('/demo/:demoId', Controller.delete);
 }
